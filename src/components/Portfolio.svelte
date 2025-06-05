15e45fcb89185ea7759e15b1d3f1a6a1c83cec21
Juan Pablo Villegas <jp@Juans-MacBook-Air.local>
2025-06-04 21:18:32 -0600

    contact added

--- src/components/Portfolio.svelte
+++ src/components/Portfolio.svelte
@@ -6,22 +6,26 @@
     {
       video: '/assets/videos/hero.mp4',
       category: 'Wedding | Summer',
-      title: 'Ena & Robert'
+      title: 'Ena & Robert',
+      date: 'June 3rd, 2025'
     },
     {
       video: '/assets/videos/hero.mp4',
       category: 'Wedding | Summer',
-      title: 'Clara & Max'
+      title: 'Clara & Max',
+      date: 'June 3rd, 2025'
     },
     {
       video: '/assets/videos/hero.mp4',
       category: 'Wedding | Summer',
-      title: 'Anne & Sven'
+      title: 'Anne & Sven',
+      date: 'June 3rd, 2025'
     },
     {
       video: '/assets/videos/hero.mp4',
       category: 'Wedding | Summer',
-      title: 'Hanna & Simon'
+      title: 'Hanna & Simon',
+      date: 'June 3rd, 2025'
     }
   ];
 
@@ -51,11 +55,11 @@
   });
 </script>
 
-<section class="bg-[#f6f2ed] pt-32 pb-16 px-4">
+<section class="bg-[#f6f2ed] pt-24 md:pt-32 pb-16 px-4">
   <div class="max-w-screen-lg mx-auto">
     <h2 class="text-4xl md:text-5xl font-main mb-6 text-black text-center">Gallery</h2>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8">
       {#each items as item}
         <div
           class="bg-white overflow-hidden rounded shadow-sm cursor-pointer"
@@ -66,15 +70,16 @@
         >
           <div class="overflow-hidden relative">
             <div class="absolute inset-0 bg-black bg-opacity-30 z-2"></div>
-            <div class="absolute inset-0 z-30 flex items-center justify-center">
-              <Icon size="w-32 h-32" color="text-white" d="M8 18V6l8 6-8 6Z" />
+            <div class="absolute inset-0 z-3 flex items-center justify-center">
+              <Icon size="w-24 h-24" color="text-white" d="M8 18V6l8 6-8 6Z" className="bg-white rounded-full bg-opacity-20 py-2 pl-2"/>
             </div>
             <video muted playsinline class="w-full h-auto object-cover">
               <source src={item.video} type="video/mp4" />
             </video>
           </div>
           <div class="p-4">
-            <p class="text-xs text-gray-500 uppercase">{item.category}</p>
+            <p class="text-xs pb-2">{item.date}</p>
+            <p class="text-xs text-gray-500 uppercase text-accent">{item.category}</p>
             <h3 class="text-lg font-medium text-gray-800 mt-1">{item.title}</h3>
           </div>
         </div>
@@ -83,42 +88,44 @@
   </div>
 
   {#if modalOpen}
-  <!-- Fondo del modal: clic aquí cierra -->
   <div
-    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70"
+    class="fixed inset-0 z-50 flex items-center justify-center md:bg-black bg-opacity-90"
     role="button"
     tabindex="0"
-    on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && closeModal()}
+    on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && closeModal}
     on:click={closeModal}
   >
-    <!-- Contenido del modal: clic aquí NO cierra -->
     <div
-      class="relative bg-white rounded-lg overflow-hidden shadow-lg max-w-3xl w-full"
+      class="relative w-screen h-screen md:max-w-3xl md:h-auto bg-black"
       role="button"
       tabindex="0"
-      on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && stopPropagation()}
+      on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && stopPropagation}
       on:click|stopPropagation
     >
+      <!-- Botón de cerrar -->
       <button
         type="button"
-        class="absolute top-2 right-2 text-white hover:text-gray-900 text-xl z-20"
+        class="absolute top-4 right-4 text-white hover:text-gray-300 text-3xl z-20"
         aria-label="Cerrar video"
         on:click={closeModal}
       >
         ✕
       </button>
 
+      <!-- Video full screen -->
       <video
         bind:this={modalVideoRef}
         src={selectedVideo}
         autoplay
         controls
-        class="w-full h-auto"
+        class="w-full h-full object-cover"
       >
         <track kind="captions" src="" srclang="en" label="Captions" />
       </video>
     </div>
   </div>
 {/if}
+
+
 
 </section>
